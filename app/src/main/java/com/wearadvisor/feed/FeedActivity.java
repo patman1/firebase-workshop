d6e218c2a2775437ab4eb6daa358600ce0152a1e
Łukasz Patecki <patman@patman.local>
2016-12-10 12:30:49 +0100

    Open profile activity

--- app/src/main/java/com/wearadvisor/feed/FeedActivity.java
+++ app/src/main/java/com/wearadvisor/feed/FeedActivity.java
@@ -1,9 +1,14 @@
 package com.wearadvisor.feed;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
+import com.wearadvisor.ProfileActivity;
 import com.wearadvisor.R;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class FeedActivity extends AppCompatActivity {
 
@@ -11,5 +16,11 @@ public class FeedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.open_profile)
+    void openProfile() {
+        startActivity(new Intent(this, ProfileActivity.class));
     }
 }
