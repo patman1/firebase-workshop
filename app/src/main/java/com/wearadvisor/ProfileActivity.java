3cb36e56e0dc6853390ca6309a39a63efbf07bb2
Łukasz Patecki <patman@patman.local>
2016-12-10 13:21:04 +0100

    Add possibility to change username

--- app/src/main/java/com/wearadvisor/ProfileActivity.java
+++ app/src/main/java/com/wearadvisor/ProfileActivity.java
@@ -2,12 +2,63 @@ package com.wearadvisor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.wearadvisor.storage.Callback;
+import com.wearadvisor.storage.DBConnector;
+import com.wearadvisor.storage.DBConnector.Cancellable;
+import com.wearadvisor.storage.DBConnector.EmptyCancellable;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    @BindView(R.id.user_name)
+    TextView userName;
+
+    Cancellable usernameCancellable = new EmptyCancellable();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        String uid = DBConnector.INSTANCE.getUid();
+        if (uid != null) {
+            usernameCancellable = DBConnector.INSTANCE
+                    .getUsername(uid, new Callback<String>() {
+                        @Override
+                        public void on(String username) {
+                            userName.setText(username);
+                        }
+                    });
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        usernameCancellable.cancel();
+        super.onPause();
+    }
+
+    @OnClick(R.id.change_name)
+    void updateUserName() {
+        String uid = DBConnector.INSTANCE.getUid();
+
+        if (uid != null) {
+            String username = userName
+                    .getText()
+                    .toString();
+
+            DBConnector.INSTANCE
+                    .setUserName(uid,username);
+        }
     }
 }
--- app/src/main/java/com/wearadvisor/storage/DBConnector.java
+++ app/src/main/java/com/wearadvisor/storage/DBConnector.java
@@ -2,13 +2,19 @@ package com.wearadvisor.storage;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 import com.wearadvisor.WearAdvisor;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -35,7 +41,6 @@ public enum DBConnector {
     }
 
 
-
     public void signUp(String name,
                        String pass,
                        OnFailureListener onFailureListener) {
@@ -53,6 +58,7 @@ public enum DBConnector {
 
     /**
      * null delivered means user not logged in.
+     *
      * @param uidCallback
      */
     public void addUserIdListener(final Callback<String> uidCallback) {
@@ -71,8 +77,77 @@ public enum DBConnector {
         firebaseAuth.addAuthStateListener(fireListener);
     }
 
+    @Nullable
+    public String getUid() {
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user != null) {
+            return user.getUid();
+        }
+        return null;
+    }
+
     public void removeUserIdListener(final Callback<String> uidCallback) {
         FirebaseAuth.AuthStateListener fireListener = authListeners.remove(uidCallback);
         firebaseAuth.removeAuthStateListener(fireListener);
+    }
+
+    public void setUserName(@NonNull String uid, @NonNull String name) {
+        database
+                .getReference()
+                .child("users")
+                .child(uid)
+                .setValue(name)
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        e.getCause();
+                    }
+                });
+    }
+
+    public Cancellable getUsername(@NonNull String uid, final Callback<String> callback) {
+        DatabaseReference ref = database
+                .getReference()
+                .child("users")
+                .child(uid);
+
+        return observeReference(ref, new Callback<DataSnapshot>() {
+            @Override
+            public void on(DataSnapshot dataSnapshot) {
+                callback.on(dataSnapshot.getValue(String.class));
+            }
+        });
+    }
+
+    public Cancellable observeReference(final DatabaseReference reference, final Callback<DataSnapshot> callback) {
+
+        final ValueEventListener listener = reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                callback.on(dataSnapshot);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        return new Cancellable() {
+            @Override
+            public void cancel() {
+                reference.removeEventListener(listener);
+            }
+        };
+    }
+
+    public interface Cancellable {
+        void cancel();
+    }
+
+    public static class EmptyCancellable implements Cancellable {
+        @Override
+        public void cancel() {
+        }
     }
 }
