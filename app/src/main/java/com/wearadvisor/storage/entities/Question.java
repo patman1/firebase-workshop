16395e1cd192a17e924c6d46e7b543e2774e88ae
Łukasz Patecki <patman@patman.local>
2016-12-10 13:51:35 +0100

    Add Question pojo

---
+++ app/src/main/java/com/wearadvisor/storage/entities/Question.java
@@ -0,0 +1,13 @@
+package com.wearadvisor.storage.entities;
+
+public class Question {
+
+    public String id;
+    public String text;
+    public String owner;
+    public long timestamp;
+
+//    String ownerName;
+//    long votesYes;
+//    long votesNo;
+}
